4a6d4fabcb69ca3b2a137fb8acf0bf4ac706e8b6
abhinildutt <abhinil.dutt@gmail.com>
2022-06-28 01:28:37 +0530

    updated token uri

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,10 +1,11 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
+import "./libraries/Base64.sol";
 
 contract MyEpicGame is ERC721 {
     struct CharacterAttributes {
@@ -16,7 +17,13 @@ contract MyEpicGame is ERC721 {
         uint attackDamage;
     }
 
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     CharacterAttributes[] defaultCharacters;
+
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+    mapping(address => uint256) public nftHolders;
 
     constructor(
         string[] memory characterNames,
@@ -24,6 +31,7 @@ contract MyEpicGame is ERC721 {
         uint[] memory characterHp,
         uint[] memory characterAttackDmg
     )
+    ERC721("Pokemon", "POKI")
     {
 
         for(uint i = 0; i < characterNames.length; i+= 1) {
@@ -38,6 +46,55 @@ contract MyEpicGame is ERC721 {
             CharacterAttributes memory c = defaultCharacters[i];
             console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
         }
+        _tokenIds.increment();
+    }
 
+
+        function mintCharacterNFT(uint _characterIndex) external {
+            uint256 newItemId = _tokenIds.current();
+
+            _safeMint(msg.sender, newItemId);
+
+            nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+            });
+
+            console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+            
+            nftHolders[msg.sender] = newItemId;
+
+            _tokenIds.increment();
+        }
+    
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+            '{"name": "',
+            charAttributes.name,
+            ' -- NFT #: ',
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+            charAttributes.imageURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,'} ]}'
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        
+        return output;
     }
 }
