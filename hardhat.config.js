bcb0faf6b3b3ec9ae676063117d7f8eeb2843cf0
abhinildutt <abhinil.dutt@gmail.com>
2022-06-28 15:49:47 +0530

    minted nfts and deployed to tesnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.1",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/-QeCJ-s98rekW0nSBtCGjQRxAyd_xx9h',
+      accounts: ['PRIVATE KEY'],
+    },
+  },
 };
